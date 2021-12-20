4714bd03718a5a40c279d2ae5a98ea16b3257709
hulyasekerci <hulyatirnapoglu@gmail.com>
2021-12-21 00:07:58 +0100

    added homework

---
+++ bootstrap.js
@@ -0,0 +1,24 @@
+//I spent too much time and tried many things but
+// I couldn't get proper results.
+//I'm submitting like this, sorry. I need more time to make more sense.
+// It will be better in time :)
+
+
+
+
+let button = document.getElementById('submit');
+
+button.addEventListener("click", function(){ alert("It is a scam!"); });
+
+
+const count = document.getElementById('count');
+const number = document.getElementById('number')
+
+count.addEventListener("click", function(){
+    for( var i=0; i<=10; i++){
+        console.log(`for loop number: ${i}`)
+    }
+
+})
+
+
